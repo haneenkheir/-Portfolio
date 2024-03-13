c9d9d19fd9e7010f6f55b356c2c9962aa7db07c7
haneenkheir <haneen.kheir@outlook.com>
2024-03-13 14:30:07 +0000

    added comments

--- Python projects/Genetic_tools/DNA_toolkit.py
+++ Python projects/Genetic_tools/DNA_toolkit.py
@@ -4,7 +4,7 @@ import collections
 import random
 from Genetic_components import * 
 
-
+# Function to validate sequence based on defined nucleotides in Genetic_components, returns error message if not in "Nucleotides"
 def validateSeq(dna_seq):
     """"Validates DNA sequences to check for defined nucleotides"""
     tempseq = dna_seq.upper()
@@ -13,6 +13,7 @@ def validateSeq(dna_seq):
             return "Error Invalid sequence" 
     return tempseq 
 
+# Function to count nucelotise frequency and returns a dictionary with the nucleotide counts in the genetic sequence
 def countNucFrequency(seq):
     """"Counts the frequency of nucleotides in a DNA/ RNA sequence"""
     tempFreqDict = {"A":0, "C":0, "G":0, "T":0, "U":0}
@@ -21,17 +22,21 @@ def countNucFrequency(seq):
     return tempFreqDict
     # return dict(collections.Counter(seq)) 
 
+# Function to replace any T in the DNA sequence with U 
 def transcription(seq):
     """DNA -> RNA Transcription, Replacing Thymine with Uracil"""
     return seq.replace("T", "U")
 
+# Function to replace bases with complimentary pairs from DNA_ReverseCompliment 
 def reverse_complement(seq):
     """Swapping adenine with thymine and guanine with cytosine, Reversing newly generated string"""
     return ''.join([DNA_ReverseCompliment[nuc] for nuc in seq])[::-1]
 
+# Counts GC content and returns percentage 
 def gc_content(seq):
     """" GC content in a DNA/RNA sequence"""
     return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
 
 def randDNAstr(nuc, dna_length):
     import random 
